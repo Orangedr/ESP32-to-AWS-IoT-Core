f688b56ad05fa851bb3a21305e3b067351e8047f
Orangedr <102016109+Orangedr@users.noreply.github.com>
2022-10-03 13:51:33 -0700

    Initial commit

---
+++ include/README
@@ -0,0 +1,32 @@
+
+## Purpose
+The purpose of the project is to utilize ESP32 or other Arduino series board to gather production data in the machine and bring the production site to digital level, help management to make decision and reduce cost due to down time and material waste
+With the available data online, we want the sensors to monitor machine status, part life span, to assist preventive maintenance.
+By doing this, we will be able to acquire real-time, clean, digital data, instead of hand-written data that needs to be input to the spreadsheet environment.
+
+## Prequisites:
+* Language: C++ (arduino)
+* Device: ESP32 (wrover-dev), Sensors (NPN Photoelectric, digital)
+* Software: Arduino IDE, Visual Studio Code, PlatformIO
+* IoT host: AWS Account
+* Hardware: Breadboard, Jumper Wires, USB Cable, Power Supply
+* Libraries: WiFiClientSecure, MQTTClient, ArduinoJson, ezButton
+
+## Function:
+
+This is a derivative from the AWS example code for ESP32, the code is modified to fit the project requirement. The original code section is in the reference section.
+
+**This code does a few things:** 
+1. Gather Analog signal counts from sensor at Pin 33, every object passing by the sensor will generate a pulse, the pulse will be counted and sent to AWS IoT Core at given topic
+2. Calculate RMP (Revolutions Per Minute) based on the pulse counts on Pin 32, the RMP will be sent to AWS IoT Core
+3. Gather digital reading on Pin 35, on which pin you can connect a digital sensor that display critical reading that you want to monitor
+4. The sensor data would converge to the function publishMessage(), where you can add or take away the data you want to send to AWS IoT Core, but please refer to ESP32 GPIO instruction to make sure you are using the correct pin number, you can't use ADC2 pins because of WIFI module.
+5. There are 2 pair of WiFi connection, if the first one failed it will try to connect the second one. **There may be some bugs in this because I didn't test the loop, but it works for me at 1500ms delay.**
+   
+## Caution:
+* The Pin 33 and 32 are at Pull up mode and connect to a NPN sensor in this project, change the input mode as you need.
+* The Access for AWS and WiFi are in the secret.h, change the settings as you need.
+
+## Reference:
+* [Building an AWS IoT Core device using AWS Serverless and an ESP32](https://aws.amazon.com/blogs/compute/building-an-aws-iot-core-device-using-aws-serverless-and-an-esp32) 
+* [Build Your First IoT App](https://aws.amazon.com/developer/learning/lets-build-iot/)
---
+++ src/Vanilla.cpp
@@ -0,0 +1,162 @@
+#include <Arduino.h>
+#include "secrets.h"
+#include <WiFiClientSecure.h>
+#include <MQTTClient.h>
+#include <ArduinoJson.h>
+#include "WiFi.h"
+#include <ezButton.h>
+
+// The MQTT topics that this device should publish/subscribe
+#define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
+#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+#define Machine_name "C1"
+
+// The pins that is used, please refer to ESP32 GPIO pins descriptions for details
+#define Pin1 32
+#define Pin2 35
+#define Pin3 33
+
+
+WiFiClientSecure net = WiFiClientSecure();
+MQTTClient client = MQTTClient(256);
+
+ezButton CounterSensor(Pin1);
+ezButton RPMSensor(Pin3);
+
+// the time variables for upload duration
+volatile unsigned long lastuploadTime = 0; // Records last count time for debounce
+
+// the time variables for calc RPM
+volatile unsigned long RPMTime1 = 1; // Calc RPM=60000/(t1-t2)
+volatile unsigned long RPMTime2 = 0; 
+double RPM1;
+
+
+void connectAWS()
+{
+// connect wifi use WIFI_SSID_1 and WIFI_PASSWORD_1, if not connect use WIFI_SSID_2 and WIFI_PASSWORD_2 
+ 
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID_1, WIFI_PASSWORD_1);
+
+  Serial.println("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    delay(1500);
+    Serial.print(".");
+    
+    WiFi.begin(WIFI_SSID_2, WIFI_PASSWORD_2);
+    Serial.println("Connected to Wi-Fi 2");
+    delay(1500);
+  }
+
+  // Configure WiFiClientSecure to use the AWS IoT device credentials
+  net.setCACert(AWS_CERT_CA);
+  net.setCertificate(AWS_CERT_CRT);
+  net.setPrivateKey(AWS_CERT_PRIVATE);
+
+  // Connect to the MQTT broker on the AWS endpoint we defined earlier
+  client.begin(AWS_IOT_ENDPOINT, 8883, net);
+
+  // Create a message handler
+  // client.onMessage(messageHandler);
+
+  Serial.print("Connecting to AWS IOT");
+
+  while (!client.connect(THINGNAME)) {
+    Serial.print(".");
+    delay(100);
+  }
+
+  if(!client.connected()){
+    Serial.println("AWS IoT Timeout!");
+    return;
+  }
+
+  // Subscribe to a topic
+  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+
+  Serial.println("AWS IoT Connected!");
+}
+
+void publishMessage()
+// THe core of the script, assign values to "doc" and publish to AWS
+{
+  StaticJsonDocument<200> doc;
+
+
+   /* Assigning values to the JSON document. */
+    doc["Run_time"] = millis();
+    // doc["sensor_33"] = analogRead(Pin1);
+    // doc["sensor_23"] = analogRead(12);
+    doc["Machine_name"] = Machine_name;
+    doc["productionCount"] = CounterSensor.getCount();
+    doc["analog"]=analogRead(Pin2);
+
+    // calculate RPM
+    doc["RPM1"] = RPM1;
+    RPM1=0;
+
+
+
+
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer); // print to client
+  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+}
+
+// void messageHandler(String &topic, String &payload) {
+//   Serial.println("incoming: " + topic + " - " + payload);
+
+// //  StaticJsonDocument<200> doc;
+// //  deserializeJson(doc, payload);
+// //  const char* message = doc["message"];
+// }
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("1");
+  connectAWS();
+  pinMode(Pin1, INPUT_PULLUP);
+  pinMode(Pin3, INPUT_PULLUP);
+
+  /* Setting the debounce time to 20ms. */
+  CounterSensor.setDebounceTime(20);
+  CounterSensor.setCountMode(COUNT_FALLING);
+
+
+  RPMSensor.setDebounceTime(20);
+  // RPMSensor.setCountMode(COUNT_FALLING);
+}
+
+void loop() {
+  CounterSensor.loop();
+  RPMSensor.loop();
+
+  // Serial.print("button 2 state: ");
+  // Serial.println(RPMSensor.getState());
+
+
+  /* Calculating the RPM. */
+  if (RPMSensor.isPressed()) {
+
+// Calculate RPM, used in publishMessage()
+    RPMTime2=RPMTime1;
+    RPMTime1 = millis();
+    RPM1=60000/(RPMTime1-RPMTime2);
+
+  // Serial.println(RPMTime1);
+  // Serial.println(RPMTime2);
+
+  }
+
+  /* A function that is required to be called in the loop() function. It is used to check for incoming
+  messages and to keep the connection to AWS IoT alive. */
+  client.loop();
+
+/* A timer that will publish the message every 2 seconds. */
+if (millis()-lastuploadTime > 2000) {
+    publishMessage();
+    lastuploadTime = millis();
+  }
+
+}
